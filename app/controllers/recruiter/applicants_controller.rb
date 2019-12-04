c81b3d5dfb2ca4d0c59bab577721e5c530911d83
ozroro <r.sleepyhead@gmail.com>
2019-12-04 18:15:08 +0900

    update 記事表示
    ページネーション追加

--- app/controllers/recruiter/applicants_controller.rb
+++ app/controllers/recruiter/applicants_controller.rb
@@ -4,7 +4,7 @@ class Recruiter::ApplicantsController < ApplicationController
 
     def index
     @q = current_user.applicants.ransack(params[:q])
-    @applicants = @q.result
+    @applicants = @q.result.includes(:user, :recruiter_article)
     @pagy, @applicants = pagy(@applicants)
   end
 
