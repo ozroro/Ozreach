05b6cfeec27cab756cc6a2b7b79534681e5b5426
ozroro <r.sleepyhead@gmail.com>
2020-02-13 18:46:15 +0900

    update circleci.config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
 # Railsのルートパスを求める。(RAILS_ROOT/config/unicorn.rbに配置している場合。)
-rails_root = File.expand_path('../../', __FILE__)
+RAILS_ROOT = File.expand_path('../../', __FILE__)
 # RAILS_ENVを求める。（RAILS_ENV毎に挙動を変更したい場合に使用。)
 # rails_env = ENV['RAILS_ENV'] || "development"
 
 
-ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+ENV['BUNDLE_GEMFILE'] = RAILS_ROOT + "/Gemfile"
 
 # Unicornは複数のワーカーで起動するのでワーカー数を定義
 # サーバーのメモリなどによって変更すること。
@@ -13,24 +13,23 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
 # 指定しなくても良い。
 # Unicornの起動コマンドを実行するディレクトリを指定します。
 # （記載しておけば他のディレクトリでこのファイルを叩けなくなる。）
-working_directory rails_root
+working_directory RAILS_ROOT
 
 # 接続タイムアウト時間
 timeout 30
 
 # Unicornのエラーログと通常ログの位置を指定。
-stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
-stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+stderr_path "#{RAILS_ROOT}/log/unicorn_stderr.log"
+stdout_path "#{RAILS_ROOT}/log/unicorn_stdout.log"
 
 # Nginxで使用する場合は以下の設定を行う。
-# [追記]nginx.confでソケットでなくTCP指定なのでこの部分は省略
-# listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
+listen "#{RAILS_ROOT}/tmp/sockets/.unicorn.sock"
 # とりあえず起動して動作確認をしたい場合は以下の設定を行う。
-listen 3000
+# listen 3000
 
 
 # プロセスの停止などに必要なPIDファイルの保存先を指定。
-pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+pid "#{RAILS_ROOT}/tmp/pids/unicorn.pid"
 
 # 基本的には`true`を指定する。Unicornの再起動時にダウンタイムなしで再起動が行われる。
 preload_app true
