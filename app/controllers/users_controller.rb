bd698eb0e9dbb1980e13c8c35becf19ccc588f04
ozroro <r.sleepyhead@gmail.com>
2020-02-14 01:31:49 +0900

    fix signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   private
   def user_params(type=:seeker)
     params.require("#{type}_user".to_sym).permit(:name, :email, :password, :password_confirmation)
