fdccbaea97915a03ff90066217daa1991f515cba
ozroro <r.sleepyhead@gmail.com>
2020-02-28 01:40:15 +0900

    New feature ログイン情報をクッキーの保存できるように

--- app/models/seeker/user.rb
+++ app/models/seeker/user.rb
@@ -6,6 +6,7 @@
 #  email           :string(255)      not null
 #  name            :string(255)      not null
 #  password_digest :string(255)      not null
+#  remember_digest :string(255)
 #  type            :string(255)      not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_142304) do
+ActiveRecord::Schema.define(version: 2020_02_27_144046) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_142304) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
